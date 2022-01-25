f674de786899546da08e4f22d4b972208536bd66
DamienOlivier567 <73164252+DamienOlivier567@users.noreply.github.com>
2022-01-25 13:44:06 +0100

    fini

--- index.php
+++ index.php
@@ -6,11 +6,11 @@
  * 3. Déployez sur votre serveur et testez !
  */
 
-$from = '';
-$to = '';
-$message = 'Hello World, sending a simple mail !';
+$from = 'monster.game@outlook.fr';
+$to = 'damien.olivier.do@gmail.com';
+//$message = 'Hello World, sending a simple mail !';
 // TODO Votre code ici.
-
+//mail($to, $from ,$message);
 
 /**
  * 4. Commentez le code précédent, mais gardez les variables $from et $to
@@ -24,3 +24,18 @@ $message = 'Hello World, sending a simple mail !';
  *     N'écrasez pas les valeurs déjà existantes ( s'il y en a ).
  */
 // TODO Votre code ici.
+$message = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the 
+industrys standard dummy text ever since the 1500s, when an unknown printer took a galley 
+of type and scrambled it to make a type specimen book. It has survived not only five centuries, 
+but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised 
+in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently 
+with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum';
+
+$message = wordwrap($message, 70, "\r\n");
+
+if (mail($to, $from, $message)) {
+    echo "Le message a bien ete envoyé. Merci !";
+}
+else {
+    echo "Une erreur est survenue lors de l'envoi du mail";
+}
